b3ceeae4b65d47dbbff083302f50e0667365b85f
Austine  Gwa <austinegwa64@gmail.com>
2022-07-13 15:56:50 +0300

    Add djl to project

--- djl/src/main/java/Main.java
+++ djl/src/main/java/Main.java
@@ -1,6 +1,14 @@
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDManager;
+
 public class Main {
 
     public static void main(String [] args){
+        NDManager manager = NDManager.newBaseManager();
+        NDArray x = manager.create(3f);
+        NDArray y = manager.create(2f);
+        x.add(y);
+        System.out.println(x);
 
     }
 
